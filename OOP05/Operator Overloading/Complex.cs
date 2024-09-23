beba85aa0e5074d35f48164a9701ebe7612da4f1
[200~LidoOoO~ <lidom899@gmail.com>
2024-09-24 01:58:39 +0300

    Part 01 Operators Overloading - Binary Operators

---
+++ OOP05/Operator Overloading/Complex.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP05.Operator_Overloading
+{
+    internal class Complex
+    {
+        public int Real { get; set; }
+
+        public int Imag { get; set; }
+
+
+
+        public override string ToString()
+        {
+            return $"{Real} + {Imag}i";
+        }
+
+
+        #region Operators Overloading
+        // +
+        // Non - Private , Class member Method
+
+        public static Complex operator + (Complex Left, Complex Right)
+        {
+            // 2 + 5i
+            // 5 + 4i
+            // 7 + 9i
+            return new Complex()
+            {
+                Real = (Left?.Real ?? 0) + (Right?.Real ?? 0),
+                Imag = (Left?.Imag ?? 0) + (Right?.Imag ?? 0)
+                // Left?.Real => Left == Null => Null ?? 0 
+                // Left?.Real => Left != Null => Left.Real 
+            };
+        }
+
+        public static Complex operator -(Complex Left, Complex Right)
+        {
+
+            return new Complex()
+            {
+                Real = (Left?.Real ?? 0) - (Right?.Real ?? 0),
+                Imag = (Left?.Imag ?? 0) - (Right?.Imag ?? 0)
+            };
+
+        }
+        #endregion
+
+
+    }
+}
--- OOP05/Program.cs
+++ OOP05/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace OOP05
+﻿using OOP05.Operator_Overloading;
+
+namespace OOP05
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 01 Operators Overloading - Binary Operators
+
+
+            Complex C1 = new Complex() { Real = 3 , Imag = 5};
+
+
+            Console.WriteLine(C1); // 3 + 5i
+
+            Complex C2 = new Complex() { Real = 2 , Imag = 4 };
+
+            Console.WriteLine(C2); // 2 + 4i
+
+            #region Binary Operators Overloading 
+
+            //Complex C3 = C1 + C2; // Invalid
+            //Console.WriteLine("==================");
+            //Console.WriteLine(C3); // 5 + 9i
+
+            //Complex C3 = C1 - C2;
+            //Console.WriteLine("====================");
+            //Console.WriteLine(C3);
+
+            //Complex C3 = default;
+            //C3 += C1; // C3 = Null + C1  
+
+            //Console.WriteLine(C3);
+
+
+            //C3 -= C1; // C3 = C3 - C1
+
+
+            #endregion
+            #endregion
         }
     }
 }
